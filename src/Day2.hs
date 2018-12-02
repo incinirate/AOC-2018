f97baecee1b27e3feea6ed825d6762cd37077712
Incinirate <dogescript@gmail.com>
2018-12-02 01:18:25 -0700

    Day 2

---
+++ src/Day2.hs
@@ -0,0 +1,31 @@
+import Data.List (nub, intersect)
+import Util
+
+data Score = Score { has2Letters :: Bool
+                   , has3Letters :: Bool }
+
+score :: Eq a => [a] -> Score
+score xs =
+  Score {has2Letters = 2 `elem` letterCounts, has3Letters = 3 `elem` letterCounts}
+  where letters = nub xs
+        letterCounts = length . flip filter xs . (==) <$> letters
+        has2Letters = 2 `elem` letterCounts
+        has3Letters = 3 `elem` letterCounts
+
+diff :: Eq a => [a] -> [a] -> [a]
+diff xs ys =
+  unJust <$> filter (/= Nothing) (zipWith test xs ys)
+  where test a b = if a == b then Just a else Nothing
+        unJust (Just a) = a
+
+main = do
+  input <- readInput $ Day 2
+  let ids = lines input
+      scores = score <$> ids
+      twos = length (filter has2Letters scores)
+      threes = length (filter has3Letters scores)
+
+  putStrLn $ "Checksum: " ++ show (twos * threes)
+
+  let commonChars = head [diff xs ys | xs <- ids, ys <- ids, length (diff xs ys) + 1 == length xs]
+  putStrLn $ "Differing Letter: " ++ commonChars
